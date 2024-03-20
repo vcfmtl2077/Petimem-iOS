ba05401748ab2a8b0bcea9b981c4ba9cecd5534d
yandeng63 <154352421+yandeng63@users.noreply.github.com>
2024-03-19 23:39:46 -0400

    tabView

---
+++ PetimemApp/PetimemApp/addNewPetView.swift
@@ -0,0 +1,18 @@
+//
+//  addNewPetView.swift
+//  PetimemApp
+//
+//  Created by Yan Deng on 2024-03-19.
+//
+
+import SwiftUI
+
+struct addNewPetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addNewPetView()
+}
